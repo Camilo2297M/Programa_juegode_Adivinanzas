36cf108a85048f0d0dba58d9e7204ac7bd7e6bd4
Programa_juegode_Adivinanzas <kmedina010@outlook.com>
2024-10-10 20:45:30 -0500

    updated

--- index.js
+++ index.js
@@ -18,10 +18,21 @@ if (numeroIngreso <1 || numeroIngreso >100 || isNaN(numeroIngreso)) {
 }
 if (numeroIngreso === numeroaleatorio) {
     menasaje.textContent = `!Felicitaciones Ganaste!!`
+    
 }else if( numeroIngreso < numeroaleatorio){
     menasaje.textContent = `Mas Alto ! el numero que ingresaste es muy bajo!`
 }else {
     menasaje.textContent = `Mas Bajo ! el numero que ingresaste es mayor!`
 
 }
+}
+
+function eliminar() {
+    document.getElementById(`validador`).value = ``
+    
+}
+
+function refrecar() {
+    document.getElementById(`refrecar`).value =   location.reload();
+    
 }
